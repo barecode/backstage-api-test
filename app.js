f99d82111d05a633cdd5cf1b3866a9919941f2f1
Michael Thompson <mcthomps@Michaels-MacBook-Pro.local>
2024-05-09 15:18:01 -0500

    Improve API response to help improve demo via backstage

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const app = express();
 
 const port = 8000;
 app.listen(port,()=> {
-console.log('listen port 8000');
+  console.log('listen port ' + port);
 })
 
 //create api
 app.get('/hello_world', (req,res)=>{
-res.send('Hello World');
+  var datetime = new Date();
+  res.send('Hello World, from port ' + port + ' at ' + datetime);
 })
 
